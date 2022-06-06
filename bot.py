91abca63d400a87d0cba503f8682b0fe584cb668
dmitm7 <matveevdm7@yandex.ru>
2022-06-06 19:55:10 +0300

    worker

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord_components import DiscordComponents
 from help_commands import *
 
+
 config = {
     'token': 'OTc2NDIyMjgwODk0NzY3MTE0.GCi5QJ.kS_3L2UK9xG0V-3y_f9EAqlRwihFqkB41MWT90',
     'prefix': '!',
